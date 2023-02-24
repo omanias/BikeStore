52aebb258c342a7636e1752cb274c3a13d641cba
omanias <ojmanias1985@gmail.com>
2023-02-24 20:22:40 -0300

    SDK deleted

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,7 @@ import "./main.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAxLOU9ij4E_KaQxNBUTAF-enWN_WSm1EQ",
-  authDomain: "coder-react-9ab42.firebaseapp.com",
-  projectId: "coder-react-9ab42",
-  storageBucket: "coder-react-9ab42.appspot.com",
-  messagingSenderId: "788425692210",
-  appId: "1:788425692210:web:d6cc3182b68652a387d82b",
-};
+const firebaseConfig = {};
 
 initializeApp(firebaseConfig);
 
